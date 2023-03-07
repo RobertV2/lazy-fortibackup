5f36f9ffd9eb03745b1fdfaf29140e817aa0c724
robertv2 <robert.hollander@outlook.com>
2023-03-07 22:34:31 +0100

    optimized backup script

--- fortinet-backup.ps1
+++ fortinet-backup.ps1
@@ -5,26 +5,31 @@ foreach ($fortigate in $fortigates) {
     $SSHSession = New-SSHSession -ComputerName $fortigate.ip -Port $fortigate.port_ssh -Credential $credential -AcceptKey -Verbose
 
     if ($SSHSession.Connected) {
-
+        # result output of system console mode check
         $console_mode = Invoke-SSHCommand -SSHSession $SSHSession -Command 'get system console | grep -i output'
+
         # checking the console mode (should be standard but default is more)
         if ($console_mode.output -match '.standard.') {
+            # if standard, continue
             Write-Host('console mode is standard');
         }
         else {
+            # if not standard, change to standard
             Write-Host('could not find standard in result')
             $SSHStream = New-SSHShellStream -SSHSession $SSHSession
             $SSHStream.WriteLine('config system console')
-            sleep 1 # because without this it didnt work for me
             $SSHStream.WriteLine('set output standard')
-            sleep 1 # because without this it didnt work for me
             $SSHStream.WriteLine('end')
             $SSHStream.Read()
         }
 
+        # get current date based on year, month, day - hours minutes seconds.
+        $date = Get-Date -Format yyyyMMdd-HHmmss
+        # get the current configuration by executing the show command
         $SSHResponse = Invoke-SSHCommand -SSHSession $SSHSession -Command 'show';
-        $savePath = './{0}.cfg' -f $fortigate.name
+        # set filename to $fortgate.name and add the date
+        $savePath = './{0}-{1}.cfg' -f $fortigate.name,$date
+        # store the output of the show command to a file with the filepath above
         $SSHResponse.output | Out-File -Encoding unicode -FilePath $savePath
-        # > ${fortigate.name}.cfg
     }
 }
